a41750714df56fc976cb5989afadbcb9ab56d895
Igor Kolpakov <kolpakovigorjob@yandex.ru>
2022-11-22 22:09:47 +0300

    init: start of project

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const filmsdb = require('mongoose');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const cookieParser = require('cookie-parser');
+const { errors } = require('celebrate');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
+const { PORT = 3000, MONGO_URL = 'mongodb://localhost:27017/filmsdb' } = process.env;
+
+const app = express();
+filmsdb.connect(MONGO_URL);
+
+app.use(helmet());
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // for 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+app.use(limiter);
+app.use(requestLogger);
+app.use(express.json());
+app.use(cookieParser());
+// routes
+app.use(errorLogger);
+app.use(errors());
+// errorhandler
+app.listen(PORT, () => {
+  console.log(`App listening at port ${PORT}`);
+});
