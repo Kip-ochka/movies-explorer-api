f04c886235baeda084c214b887dfe2a392f574bd
Igor Kolpakov <gragass00@yandex.ru>
2022-11-24 13:14:41 +0300

    change port

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { errorHandler } = require('./middlewares/errorHandler');
 const router = require('./routes/index');
 
-const { PORT = 3000, MONGO_URL = 'mongodb://localhost:27017/filmsdb' } = process.env;
+const { PORT = 3001, MONGO_URL = 'mongodb://localhost:27017/filmsdb' } = process.env;
 
 const app = express();
 filmsdb.connect(MONGO_URL);
