17db1d7ebd872938e21d0b3fde9cfede0e0744fd
Igor Kolpakov <gragass00@yandex.ru>
2022-11-25 11:23:54 +0300

    fix: prod patch

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,11 +1,12 @@
 const { SERVER_ERROR } = require('../utils/variables');
 
 module.exports.errorHandler = (err, _, res, next) => {
+  console.log(err);
   const { statusCode = 500, message } = err;
   res
     .status(statusCode)
     .send({
-      message: statusCode === 500 ? SERVER_ERROR : message,
+      message: statusCode === 500 ? err : message,
     });
   next();
 };
