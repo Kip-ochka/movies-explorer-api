c12e84d8cae5b40f7573a3e70b45acbbe0e95418
Igor Kolpakov <gragass00@yandex.ru>
2022-12-23 09:00:36 +0300

    fix error

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,11 +1,10 @@
-const { SERVER_ERROR } = require('../utils/variables');
+const { SERVER_ERROR } = require("../utils/variables");
 
 module.exports.errorHandler = (err, _, res, next) => {
   const { statusCode = 500, message } = err;
-  res
-    .status(statusCode)
-    .send({
-      message: statusCode === 500 ? SERVER_ERROR : message,
-    });
+  res.status(statusCode).send({
+    message: statusCode === 500 ? SERVER_ERROR : message,
+    statusCode,
+  });
   next();
 };
